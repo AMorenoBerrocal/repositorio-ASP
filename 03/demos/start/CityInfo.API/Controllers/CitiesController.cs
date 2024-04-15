4dff75827dc7a7ec1516e950bec7ba18ea1b8a11
AVANADE-CORP\a.moreno.berrocal <a.moreno.berrocal@avanade.com>
2024-04-15 10:34:15 +0200

    CitiesData - nuevos metodos get

--- 03/demos/start/CityInfo.API/Controllers/CitiesController.cs
+++ 03/demos/start/CityInfo.API/Controllers/CitiesController.cs
@@ -1,18 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.API.Controllers
 {
     [ApiController]
+    [Route("api/cities")]
+
     public class CitiesController : ControllerBase 
     {
+        [HttpGet]
         public JsonResult GetCities()
         {
-            return new JsonResult(
-                new List<Object>
-                {
-                    new {id=1 , Name = "New York City"},
-                    new {id=2 , Name = "Newstrep"}
-                });
+            return new JsonResult(CitiesDataStore.Current.Cities);
         }
+
+        
     }
 }
