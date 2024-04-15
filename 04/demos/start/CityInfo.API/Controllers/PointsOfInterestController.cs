f6bbabdf1d73d5464247851236f0f708e7023fa0
AVANADE-CORP\a.moreno.berrocal <a.moreno.berrocal@avanade.com>
2024-04-15 13:19:45 +0200

    implementacion - custom service

--- 04/demos/start/CityInfo.API/Controllers/PointsOfInterestController.cs
+++ 04/demos/start/CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -1,4 +1,5 @@
 ﻿using CityInfo.API.Models;
+using CityInfo.API.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
@@ -11,10 +12,13 @@ namespace CityInfo.API.Controllers
     {
 
         private readonly ILogger<PointsOfInterestController> _logger;
+        private readonly LocalMailService _mailService;
 
-        public PointsOfInterestController(ILogger <PointsOfInterestController> logger)
+        public PointsOfInterestController(ILogger <PointsOfInterestController> logger,
+            LocalMailService mailService)
         {
             _logger = logger ?? throw new ArgumentException(nameof(logger));
+            _mailService = mailService ?? throw new ArgumentException(nameof(mailService));
         }
 
         [HttpGet]
@@ -174,6 +178,7 @@ namespace CityInfo.API.Controllers
                 return NotFound();
             }
             city.PointsOfInterest.Remove(pointOfInterestFromStore);
+            _mailService.Send("Point of interest deleted.", $"Point of interest {pointOfInterestFromStore.Name} with id {pointOfInterestFromStore.Id} was deleted.");
             return NoContent();
         }
 
