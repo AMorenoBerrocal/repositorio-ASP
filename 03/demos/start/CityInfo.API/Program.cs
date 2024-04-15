b148ea33fffba29e927edd4ec0a210f7322da65f
AVANADE-CORP\a.moreno.berrocal <a.moreno.berrocal@avanade.com>
2024-04-15 09:51:53 +0200

    Activar controladores

--- 03/demos/start/CityInfo.API/Program.cs
+++ 03/demos/start/CityInfo.API/Program.cs
@@ -17,15 +17,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 
-//app.UseAuthorization();
+app.UseAuthorization();
 
-//app.MapControllers();
+app.MapControllers();
 
-app.Run(async (context) =>
-{
-    await context.Response.WriteAsync("Hello World!");
-});
 
 app.Run();
