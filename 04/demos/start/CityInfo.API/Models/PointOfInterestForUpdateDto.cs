38db2226efe126146daad327081dd9e8ae1cd3d2
AVANADE-CORP\a.moreno.berrocal <a.moreno.berrocal@avanade.com>
2024-04-15 11:49:17 +0200

    Modificar un recurso

---
+++ 04/demos/start/CityInfo.API/Models/PointOfInterestForUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace CityInfo.API.Models
+{
+    public class PointOfInterestForUpdateDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
