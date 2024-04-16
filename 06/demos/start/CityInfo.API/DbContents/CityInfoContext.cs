6f2cfcb5b100c67b73cf72c11e39aec9ab2eff8d
AVANADE-CORP\a.moreno.berrocal <a.moreno.berrocal@avanade.com>
2024-04-16 09:50:05 +0200

    Nuevo: dbContext

---
+++ 06/demos/start/CityInfo.API/DbContents/CityInfoContext.cs
@@ -0,0 +1,16 @@
+﻿using CityInfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.API.DbContents
+{
+    public class CityInfoContext : DbContext 
+    {
+        public DbSet<City> Cities { get; set; }
+        public DbSet<PointOfInterest> PointOfInterests { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionBui
+        //}
+    }
+}
