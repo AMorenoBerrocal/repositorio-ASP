e87a6575101f04e56d47285d76d466a6d9cb8cb8
AVANADE-CORP\a.moreno.berrocal <a.moreno.berrocal@avanade.com>
2024-04-16 11:55:44 +0200

    Repository pattern

---
+++ 07/demos/start/CityInfo.API/Services/ICityInfoRepository.cs
@@ -0,0 +1,13 @@
+﻿using CityInfo.API.Entities;
+
+namespace CityInfo.API.Services
+{
+    public interface ICityInfoRepository
+    {
+        Task<IEnumerable<City>> GetCities();
+        Task<City?> GetCitiesAsync(int cityId, bool includePointsOfInterest);
+
+        Task<PointOfInterest?> GetPointOfInterestForCityAsync(int cityId,
+            int pointOfInterestId);
+    }
+}
